4eeabeb9126c54c1046bf357a9cfd7cdaea40647
AchmadPR <96861665+AchmadPR@users.noreply.github.com>
2022-03-16 10:12:47 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ else
 echo "Lanjut mase"
 fi
 wget --no-check-certificate "https://github.com/AchmadPR/Config-Editor-OC/blob/main/luci-app-tinyfm_1.6_all.ipk" -O /root/fm.ipk
-cd ~/ && opkg install fm.ipk --force-depends
+opkg install fm.ipk --force-depends
 wget --no-check-certificate "https://github.com/AchmadPR/Config-Editor-OC/blob/main/openclash.lua" -O /usr/lib/lua/luci/controler/openclash.lua
 wget --no-check-certificate "https://github.com/AchmadPR/Config-Editor-OC/blob/main/editor.htm" -O /usr/lib/lua/luci/view/openclash/editor.htm
 wget --no-check-certificate "https://github.com/AchmadPR/Config-Editor-OC/blob/main/yacd.htm" -O /usr/lib/lua/luci/view/openclash/yacd.htm
