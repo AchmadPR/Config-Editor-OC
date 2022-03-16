61fc7ef99f4f624b6c163c3da6c416b6ab20ae29
AchmadPR <96861665+AchmadPR@users.noreply.github.com>
2022-03-16 10:58:02 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ cd ~/ && opkg update
 echo "Wes lebar nembe lanjut mase"
 wget --no-check-certificate "https://github.com/AchmadPR/Config-Editor-OC/blob/main/tinyfm_1.6_all.ipk?raw=true" -O ~/fm.ipk
 opkg install --force-depends *.ipk
-wget --no-check-certificate "https://github.com/AchmadPR/Config-Editor-OC/blob/main/openclash.lua" -O /usr/lib/lua/luci/controler/openclash.lua
+wget --no-check-certificate "https://github.com/AchmadPR/Config-Editor-OC/blob/main/openclash.lua" -O /usr/lib/lua/luci/controller/openclash.lua
 wget --no-check-certificate "https://github.com/AchmadPR/Config-Editor-OC/blob/main/editor.htm" -O /usr/lib/lua/luci/view/openclash/editor.htm
 wget --no-check-certificate "https://github.com/AchmadPR/Config-Editor-OC/blob/main/yacd.htm" -O /usr/lib/lua/luci/view/openclash/yacd.htm
 wget --no-check-certificate "https://github.com/AchmadPR/Config-Editor-OC/blob/main/editor.php" -O /www/tinyfm/cdn/editor.php
