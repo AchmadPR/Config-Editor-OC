97a6573b411032edae22df10f25b76f46d80742a
AchmadPR <96861665+AchmadPR@users.noreply.github.com>
2022-03-16 09:29:26 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #tinyfm (AchmadPR)
 cd ~/ && opkg update
-wget --no-check-certificate "https://raw.githubusercontent.com/AchmadPR/Config-Editor-OC/luci-app-tinyfm_1.6_all.ipk" -O /tmp/luci-app-tinyfm_1.6_all.ipk
+wget --no-check-certificate "https://github.com/AchmadPR/Config-Editor-OC/blob/main/luci-app-tinyfm_1.6_all.ipk" -O /tmp/luci-app-tinyfm_1.6_all.ipk
 cd /tmp ~/ opkg install --force-depends *.ipk
 cd ~/
-wget --no-check-certificate "https://raw.githubusercontent.com/AchmadPR/Config-Editor-OC/openclash.lua" -O /usr/lib/lua/luci/controler/openclash.lua
-wget --no-check-certificate "https://raw.githubusercontent.com/AchmadPR/Config-Editor-OC/editor.htm" -O /usr/lib/lua/luci/view/openclash/editor.htm
-wget --no-check-certificate "https://raw.githubusercontent.com/AchmadPR/Config-Editor-OC/yacd.htm" -O /usr/lib/lua/luci/view/openclash/yacd.htm
-wget --no-check-certificate "https://raw.githubusercontent.com/AchmadPR/Config-Editor-OC/editor.php" -O /www/tinyfm/cdn/editor.php
+wget --no-check-certificate "https://github.com/AchmadPR/Config-Editor-OC/blob/main/openclash.lua" -O /usr/lib/lua/luci/controler/openclash.lua
+wget --no-check-certificate "https://github.com/AchmadPR/Config-Editor-OC/blob/main/editor.htm" -O /usr/lib/lua/luci/view/openclash/editor.htm
+wget --no-check-certificate "https://github.com/AchmadPR/Config-Editor-OC/blob/main/yacd.htm" -O /usr/lib/lua/luci/view/openclash/yacd.htm
+wget --no-check-certificate "https://github.com/AchmadPR/Config-Editor-OC/blob/main/editor.php" -O /www/tinyfm/cdn/editor.php
 echo "Auto Skrip by Achmad PR"
 echo "Sampon lebar mase, monggo di reboot"
 
