5e2cfac9d3bc3a567b168edd2f8d3706a09fbb47
AchmadPR <96861665+AchmadPR@users.noreply.github.com>
2022-03-16 11:36:55 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 #tinyfm (AchmadPR)?raw=true
-echo "Update dulu mase biar lancar"
+echo "Auto Installer Config Editor Openclash by Achmad PR"
 cd ~/ && opkg update
-echo "Wes lebar nembe lanjut mase"
 wget --no-check-certificate "https://github.com/AchmadPR/Config-Editor-OC/blob/main/tinyfm_1.6_all.ipk?raw=true" -O ~/fm.ipk
 opkg install --force-depends *.ipk
 wget --no-check-certificate "https://raw.githubusercontent.com/AchmadPR/Config-Editor-OC/main/openclash.lua" -O /usr/lib/lua/luci/controller/openclash.lua
 wget --no-check-certificate "https://raw.githubusercontent.com/AchmadPR/Config-Editor-OC/main/editor.htm" -O /usr/lib/lua/luci/view/openclash/editor.htm
 wget --no-check-certificate "https://raw.githubusercontent.com/AchmadPR/Config-Editor-OC/main/yacd.htm" -O /usr/lib/lua/luci/view/openclash/yacd.htm
+wget --no-check-certificate "https://raw.githubusercontent.com/AchmadPR/Config-Editor-OC/main/myip.htm" -O /usr/lib/lua/luci/view/openclash/myip.htm
+wget --no-check-certificate "https://raw.githubusercontent.com/AchmadPR/Config-Editor-OC/main/developer.htm" -O /usr/lib/lua/luci/view/openclash/developer.htm
 wget --no-check-certificate "https://raw.githubusercontent.com/AchmadPR/Config-Editor-OC/main/editor.php" -O /www/tinyfm/cdn/editor.php
-echo "Auto Skrip by Achmad PR"
-echo "Sampon lebar mase, monggo di reboot"
+echo "Instalasi Selesai, silahkan reboot perangkat anda"
+echo "Auto Installer Config Editor Openclash by Achmad PR"
+
 
