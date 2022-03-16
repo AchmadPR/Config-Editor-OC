c8da515b73b8acd0765f1b40e83b8a916417a2be
AchmadPR <96861665+AchmadPR@users.noreply.github.com>
2022-03-16 10:10:48 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 #tinyfm (AchmadPR)
+else
 echo "Update dulu mase"
+fi
 cd ~/ && opkg update
+else
 echo "Lanjut mase"
-wget --no-check-certificate "https://github.com/AchmadPR/Config-Editor-OC/blob/main/luci-app-tinyfm_1.6_all.ipk" -O /root/luci-app-tinyfm_1.6_all.ipk
-cd ~/ && opkg install luci-app-tinyfm_1.6_all.ipk --force-depends
+fi
+wget --no-check-certificate "https://github.com/AchmadPR/Config-Editor-OC/blob/main/luci-app-tinyfm_1.6_all.ipk" -O /root/fm.ipk
+cd ~/ && opkg install fm.ipk --force-depends
 wget --no-check-certificate "https://github.com/AchmadPR/Config-Editor-OC/blob/main/openclash.lua" -O /usr/lib/lua/luci/controler/openclash.lua
 wget --no-check-certificate "https://github.com/AchmadPR/Config-Editor-OC/blob/main/editor.htm" -O /usr/lib/lua/luci/view/openclash/editor.htm
 wget --no-check-certificate "https://github.com/AchmadPR/Config-Editor-OC/blob/main/yacd.htm" -O /usr/lib/lua/luci/view/openclash/yacd.htm
