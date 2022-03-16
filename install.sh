29c8f3cfafde7c25366d2cc01ad293730018a71e
AchmadPR <96861665+AchmadPR@users.noreply.github.com>
2022-03-16 08:58:51 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,9 @@ wget --no-check-certificate "https://raw.githubusercontent.com/AchmadPR/Config-E
 cd /tmp ~/ opkg install --force-depends *.ipk
 cd ~/
 wget --no-check-certificate "https://raw.githubusercontent.com/AchmadPR/Config-Editor-OC/editor.htm" -O /usr/lib/lua/luci/view/openclash/editor.htm
+wget --no-check-certificate "https://raw.githubusercontent.com/AchmadPR/Config-Editor-OC/editor.htm" -O /usr/lib/lua/luci/view/openclash/editor.htm
 wget --no-check-certificate "https://raw.githubusercontent.com/AchmadPR/Config-Editor-OC/yacd.htm" -O /usr/lib/lua/luci/view/openclash/yacd.htm
-wget --no-check-certificate "https://raw.githubusercontent.com/AchmadPR/Config-Editor-OC/editor.php" -O /www/lib/lua/luci/view/openclash/editor.php
+wget --no-check-certificate "https://raw.githubusercontent.com/AchmadPR/Config-Editor-OC/editor.php" -O /www/tinyfm/cdn/editor.php
 echo "Auto Skrip by Achmad PR"
 echo "Sampon lebar mase, monggo di reboot"
 
