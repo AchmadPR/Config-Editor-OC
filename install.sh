ddfc76f12773469d8754a9ad4e8768aa94d3d065
AchmadPR <96861665+AchmadPR@users.noreply.github.com>
2022-03-16 08:42:23 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 #tinyfm (AchmadPR)
+cd ~/ && opkg update
+wget --no-check-certificate "https://raw.githubusercontent.com/AchmadPR/Config-Editor-OC/luci-app-tinyfm_1.6_all.ipk" -O /tmp/luci-app-tinyfm_1.6_all.ipk
+opkg install --force-depends *.ipk
+wget --no-check-certificate "https://raw.githubusercontent.com/AchmadPR/Config-Editor-OC/editor.htm" -O /usr/lib/lua/luci/view/openclash/editor.htm
+wget --no-check-certificate "https://raw.githubusercontent.com/AchmadPR/Config-Editor-OC/yacd.htm" -O /usr/lib/lua/luci/view/openclash/yacd.htm
+wget --no-check-certificate "https://raw.githubusercontent.com/AchmadPR/Config-Editor-OC/editor.php" -O /www/lib/lua/luci/view/openclash/editor.php
+echo "Auto Skrip by Achmad PR"
+echo "Sampon lebar, monggo di reboot"
+
